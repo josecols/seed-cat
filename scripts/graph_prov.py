c465fe2b0844b1c0bfa5d1a4dcdc4ded635eac6b
Jose Cols <josecolsg@gmail.com>
2024-09-21 12:06:22 -0700

    Add support for exporting graphs in SVG and PDF.

--- scripts/graph_prov.py
+++ scripts/graph_prov.py
@@ -4,7 +4,7 @@ import prov.model as prov
 from prov.dot import prov_to_dot
 
 
-def main(json_file):
+def main(json_file, output_format):
     with open(json_file, "r") as f:
         doc = prov.ProvDocument.deserialize(content=f.read(), format="json")
         dot = prov_to_dot(
@@ -14,7 +14,15 @@ def main(json_file):
             show_element_attributes=False,
         )
         dot.set("dpi", "300")
-        dot.write_png("prov.png")
+
+        output_file = f"prov.{output_format}"
+
+        if output_format in ["png", "pdf", "svg"]:
+            getattr(dot, f"write_{output_format}")(output_file)
+        else:
+            raise ValueError(f"Unsupported output format: {output_format}")
+
+        print(f"Graph saved as {output_file}")
 
 
 if __name__ == "__main__":
@@ -22,5 +30,12 @@ if __name__ == "__main__":
         description="Generate a graph image from a PROV-JSON file."
     )
     parser.add_argument("json_file", type=str, help="Path to the PROV-JSON file.")
+    parser.add_argument(
+        "-f", "--format",
+        type=str,
+        choices=["png", "pdf", "svg"],
+        default="pdf",
+        help="Output format (default: pdf)"
+    )
     args = parser.parse_args()
-    main(args.json_file)
+    main(args.json_file, args.format)
